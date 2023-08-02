a4cdafd6f8fa4ce1c346c63496c59603e55e837b
Ramkumar Ramachandran <ramkumarramachandran0@gmail.com>
2023-08-02 22:51:54 +0530

    Initial commit

--- timeSeriesAnalysis.py
+++ timeSeriesAnalysis.py
@@ -18,5 +18,14 @@ def exploreInputs():
     for dirname,_, filenames in os.walk('./input'):
         for filename in filenames:
             print(filename)
-            
+    BASE = './input/'
+
+    train = pd.read_csv(BASE + "train.csv")
+    test = pd.read_csv(BASE + "test.csv")
+    oil = pd.read_csv(BASE + "oil.csv")
+    stores = pd.read_csv(BASE + "stores.csv")
+    transactions = pd.read_csv(BASE + "transactions.csv")
+    holidays_events = pd.read_csv(BASE + "holidays_events.csv")
+    sample_submission = pd.read_csv(BASE + "sample_submission.csv") 
+        
 exploreInputs()       
